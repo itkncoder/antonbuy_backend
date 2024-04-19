37f959190d3c53ee95e62ad1e8cf81edaba54cee
itkncoder <kncoderr@gmail.com>
2024-04-19 21:10:05 +0500

    f

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const mongodb = require('./utils/mongoose-util');
-
+var cors = require('cors')
 const carsRouter = require('./routes/cars-routes');
 
 // setup
 const app = express();
 dotenv.config();
 
-
 // app configs
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+app.use(cors())
 
 // app routes
 app.use('/api/cars', carsRouter)
